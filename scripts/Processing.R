81be66825ce18e1f23ab5438687d3cfe37fa5397
CrBridge <91331576+CrBridge@users.noreply.github.com>
2024-05-11 12:14:42 +0100

    Create is_debut column
    Column that states if the album is that artists debut. Assumes no albums here are non-debut albums that were released in the same year as the debut

--- scripts/Processing.R
+++ scripts/Processing.R
@@ -4,8 +4,6 @@ library(tidyverse)
 
 rolling_stone <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-05-07/rolling_stone.csv')
 
-View(rolling_stone)
-
 #listing rows where years_between is 0, shows all debut albums in the data
 #Assumes there's no edge case where an artist released another album the same year as their debut
 
@@ -15,3 +13,13 @@ View(rolling_stone)
 #Look for typos and such (example, one entry has genre Blues/ROck)
 
 #Remember to use $ for variables
+
+# Create 'is_debut' boolean variable: True if years_between = 0
+
+rolling_stone$is_debut <- ifelse(rolling_stone$years_between == 0, 1, 0)
+
+View(rolling_stone)
+
+#5 albums in the data are by various artists
+#causes NA values in artist details and other columns
+#I probably drop these 5 albums, cant imagine why I would want them

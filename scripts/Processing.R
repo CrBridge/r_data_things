d025bcac2691cf486f8e899b5fca7beee3a9a7e0
CrBridge <91331576+CrBridge@users.noreply.github.com>
2024-05-11 12:47:13 +0100

    Replace typo in genre column

--- scripts/Processing.R
+++ scripts/Processing.R
@@ -12,6 +12,7 @@ rolling_stone <- rolling_stone[!grepl('Various Artists', rolling_stone$clean_nam
   #gotta figure out what to do with them (For full list use colSums(is.na(df))])
 
 #Look for typos and such (example, one entry has genre Blues/ROck)
+rolling_stone$genre <- str_replace(rolling_stone$genre, "Blues/Blues ROck", "Blues/Blues Rock")
 
 # Create 'is_debut' Boolean variable: True if years_between = 0
 #Assumes there's no edge case where an artist released another album the same year as their debut
@@ -19,3 +20,11 @@ rolling_stone <- rolling_stone[!grepl('Various Artists', rolling_stone$clean_nam
 rolling_stone$is_debut <- ifelse(rolling_stone$years_between == 0, 1, 0)
 
 View(rolling_stone)
+
+#If album didn't appear on billboard top 200, peak position is set to 201
+
+#Columns with NA, spotify columns, genre, rank columns and weeks on billboard
+
+#To use member_count as categorical data, need to convert to factor
+  #Before I do that, is there any reason I would want them as numbers?
+    #Could also use a temp value if I just want to convert for plots

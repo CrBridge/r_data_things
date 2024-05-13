8890d1ef273d7b699a045f7094054d959fae7c6f
CrBridge <C.Bridge2@newcastle.ac.uk>
2024-05-14 00:17:39 +0100

    Create plot showing change in genre ranking
    Uses pivoted genre table to display each genre and whether they ranked higher or lower on average year by year. Probably the last commit for now as I want to check out the new data. Probably gonna come back to this at some point though.

--- scripts/Plotting.R
+++ scripts/Plotting.R
@@ -1,7 +1,22 @@
-library(tidyverse)
+#Might want to separate each plot into a separate script, we'll see
 
-rolling_stone <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-05-07/rolling_stone.csv')
+#Run Processing.R to clean data beforehand
+#might want to load 'ggthemes' for more customization
+library(ggthemes)
+# gender of artists by genre
+(rolling_stone |> drop_na(genre)) |>
+  ggplot(aes(y = fct_infreq(genre), fill = artist_gender)) + geom_bar()
 
-g <- ggplot(rolling_stone, aes(y = genre))
-# Number of cars in each class:
-g + geom_bar(aes(fill = artist_gender))
+#g + geom_bar(aes(fill = as.factor(artist_member_count)))
+#ggplot(rolling_stone, aes(y = artist_gender)) + geom_bar()
+#ggplot(genre_change_df, aes(x=genre_ave_billboard_weeks, y=genre_ave_billboard_ranks, label=genre)) +
+ #geom_point(size = 5) # Show dots
+#ggplot(rolling_stone, aes(x=peak_billboard_position, y=weeks_on_billboard)) +
+  #geom_point(alpha=0.1) +
+  #geom_smooth(se=FALSE)
+
+#Introducing line width and line type causes some genres to have no lines at all
+ggplot(genres_pivoted, aes(x = year, y = avg_rank)) +
+  geom_line(aes(color = genre, , linetype = genre), linewidth = 1) +
+  geom_point(aes(color = genre), size = 3, alpha = 0.4) +
+  scale_x_continuous(breaks = c(2003, 2020))
